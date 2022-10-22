b7142b98435219316da9c095cc532f1625b6daaf
Jawad Erfani <jawad.erfani147@gmail.com>
2022-10-22 09:27:47 +0430

    Story class added

--- lib/main.dart
+++ lib/main.dart
@@ -23,21 +23,19 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
-  StoryBrain first =StoryBrain();
+  StoryBrain first = StoryBrain();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: 450,
         width: double.infinity,
-        //TODO: Step 1 - Add background.png to this Container as a background image.
-         decoration: BoxDecoration(
-           image: DecorationImage(image: AssetImage('images/background.png'),
-             fit: BoxFit.cover,
-
-           ),
-         ),
-
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -65,7 +63,6 @@ class _StoryPageState extends State<StoryPage> {
                     });
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
@@ -85,7 +82,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this TextButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child:Visibility(
+                child: Visibility(
                   visible: first.buttonShouldBeVisible(),
                   child: TextButton(
                     onPressed: () {
