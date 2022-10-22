41a2058353693bafe58a31a2077d653b48495e35
Jawad Erfani <jawad.erfani147@gmail.com>
2022-10-22 09:30:51 +0430

    some fixing on main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:math';
 import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -15,8 +13,6 @@ class Destini extends StatelessWidget {
     );
   }
 }
-
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -46,7 +42,6 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     first.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -61,8 +56,6 @@ class _StoryPageState extends State<StoryPage> {
                     setState(() {
                       first.nextStory(Random().nextInt(6));
                     });
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
